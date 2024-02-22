3e2f45836587593339ccb9169baa9f9ec6ba10b2
‘LeandroAndrade’ <profeleandro@outlook.com>
2024-02-22 07:44:52 -0300

    feat: base

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="author" content="Setor Informática Educativa e Leandro Andrade">
+        <link rel="shortcut icon" href="{{ asset('storage/icon.png') }}">
 
         <title>{{ config('app.name', 'Laravel') }} | {{ isset($title) ? $title : 'Painel' }}</title>
+
+        <!-- Icon -->
+        <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -22,7 +28,7 @@
     <body class="font-sans antialiased">
         <x-banner />
 
-        <div class="max-h-full min-h-screen bg-opacity-75 bg-cyan-700 dark:bg-gray-900">
+        <div class="max-h-full min-h-screen bg-opacity-75 bg-neutral-400 dark:bg-gray-900">
             @livewire('navigation-menu')
 
             <!-- Page Content -->
--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -4,8 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="author" content="Setor Informática Educativa e Leandro Andrade">
+        <link rel="shortcut icon" href="{{ asset('storage/icon.png') }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }} | {{ isset($title) ? $title : 'Painel' }}</title>
+
+        <!-- Icon -->
+        <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -20,7 +26,7 @@
         @livewireStyles
     </head>
     <body>
-        <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <div class="font-sans antialiased text-gray-900 dark:text-gray-100">
             {{ $slot }}
         </div>
 
